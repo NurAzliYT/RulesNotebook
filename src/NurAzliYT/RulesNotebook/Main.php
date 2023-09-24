8150bcb864628b6986aeb60422e68a863f96508b
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-24 16:19:56 +0700

    Update Main.php

--- src/NurAzliYT/RulesNotebook/Main.php
+++ src/NurAzliYT/RulesNotebook/Main.php
@@ -1,1 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NurAzliYT\RulesNoteBook;
+
+use cooldogedev\libBook\LibBook;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\item\VanillaItems;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+
+	protected function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveDefaultConfig();
+		LibBook::register($this);
+	}
+
+	private function sendPreview(Player $player): void {
+		$item = VanillaItems::WRITTEN_BOOK();
+		foreach ($this->getConfig()->get("pages") as $pageId => $pageText) {
+			$page = intval(explode("_", $pageId)[1]) - 1;
+			$item->setPageText($page, TextFormat::colorize($pageText));
+		}
+		LibBook::sendPreview($player, $item);
+	}
+
+	public function onJoin(PlayerJoinEvent $event): void {
+		$player = $event->getPlayer();
+		if ($player->hasPlayedBefore()) {
+			return;
+		}
+		$this->sendPreview($player);
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+		if ($command->getName() === "rulesnotebook") {
+			if (!$sender instanceof Player) {
+				$sender->sendMessage(TextFormat::RED . "You can't use this command in the terminal!");
+				return true;
+			}
+			$this->sendPreview($sender);
+			return true;
+		}
+		return false;
+	}
+}
 
